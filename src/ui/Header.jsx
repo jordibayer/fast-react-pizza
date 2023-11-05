481b6a4f21e55820438efbd3955b6d98f1a0744c
Jordi <jordi.be.1992@gmail.com>
2023-11-05 13:50:21 +0100

    23.5

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import Username from '../features/user/Username';
 import SearchOrder from '../features/order/SearchOrder';
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>User</p>
+      <Username />
     </header>
   );
 }
